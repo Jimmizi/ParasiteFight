e46df9726fc41f4cbfae92907e71c260f88edc3e
JakeJimmyThorne <jake2.thorne@live.uwe.ac.uk>
2015-01-23 19:09:53 +0000

    Player shooting

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+    public ParticleSystem ProjectileHit;
+
+    public void Initialise(Sprite spr)
+    {
+        this.GetComponent<SpriteRenderer>().sprite = spr;
+    }
+
+    //Called when the bullet spawns in from the weapon class
+    public void Shoot(float angle, float force)
+    {
+        Vector3 dir = Quaternion.AngleAxis(angle, Vector3.forward) * Vector3.right;
+        this.rigidbody2D.AddForce(dir * force);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //Explode logic
+
+        /* Spawn particle system
+         * Deform terrain
+         * Damage characters
+         */
+
+        Instantiate(ProjectileHit, this.transform.position,Quaternion.identity);
+
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+	public string weaponName;
+
+	public float weaponDamage;
+    public float weaponSpeed;
+
+    public Sprite WeaponTexture;
+    public Sprite ProjectileTexture;
+
+    public GameObject ProjectilePrefab;
+
+	void Start () 
+	{
+        weaponSpeed = 300;
+	}
+
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+	}
+
+    void Shoot()
+    {
+        GameObject go = (GameObject)Instantiate(ProjectilePrefab, this.transform.position, Quaternion.identity);
+        go.GetComponent<Projectile>().Initialise(ProjectileTexture);
+        go.AddComponent<PolygonCollider2D>();
+
+        go.GetComponent<Projectile>().Shoot(45, weaponSpeed);
+    }
+
+}
