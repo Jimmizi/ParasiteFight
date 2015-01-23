3ab179639e55d4f4dde08e4d637f8540b6755b45
Vin Teoh <vintech2706@live.com>
2015-01-23 20:52:19 +0000

    Camera movement in X-axis 完成する！！必勝！！

---
+++ Assets/Scripts/MathHelper.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MathHelper : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public Vector3 Truncate(Vector3 input, float max)
+    {
+        float i = 0.0f;
+        float length = Vector3.Magnitude(input);
+
+        i = max/length;
+        i = i < 1.0f ? i : 1.0f;
+
+        Vector3 v = Vector3.zero;
+        v = v*i;
+
+        return v;
+    }
+}
