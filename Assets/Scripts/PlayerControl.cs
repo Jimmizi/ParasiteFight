8c17b270903b725c1749e36cdd1ced5366566451
Vin Teoh <vintech2706@live.com>
2015-01-24 04:46:58 +0000

    Contains single giblet that can bounce and has angular rotation. Player worm thing moves with rigidbody 2D and has collision with terrain. Physics tweaked and improved a bit.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -6,12 +6,12 @@ using System.Threading;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float Mass = 1.0f;
-    public float DragCoefficient = -10.0f;
+    //public float Mass = 1.0f;
+    //public float DragCoefficient = 100.0f;
 
     private bool m_isAllowJump = false;
     private const float MAX_FORCE = 10.0f;
-    private const float MAX_SPEED = 10.0f;
+    private const float MAX_SPEED = 5.0f;
 
     private float m_timer = 0.0f;
 
@@ -27,16 +27,17 @@ public class PlayerControl : MonoBehaviour
         m_mathScript = GetComponent<MathHelper>();
     }
 
+
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKey(KeyCode.D))
-            rigidbody2D.AddForce(new Vector2(10.0f, 0.0f));
+            rigidbody2D.AddForce(new Vector2(1.0f, 0.0f), ForceMode2D.Impulse);
         else if(Input.GetKey(KeyCode.A))
-            rigidbody2D.AddForce(new Vector2(-10.0f, 0.0f));
+            rigidbody2D.AddForce(new Vector2(-1.0f, 0.0f), ForceMode2D.Impulse);
 
         // If player's velocity is not going in Y, allow 
-        if (rigidbody2D.velocity.y < 10.0f)
+        if (rigidbody2D.velocity.y < 6.0f)
         {
             if (m_isAllowJump)
             {
@@ -48,16 +49,14 @@ public class PlayerControl : MonoBehaviour
         }
         
         // Do check of height
-        if(rigidbody2D.velocity.y > 9.9999f)
-        {
+        if(rigidbody2D.velocity.y > 5.9999f)
             m_isAllowJump = false;
-        }
         if (rigidbody2D.velocity.y == 0.0f)
-        {
             m_isAllowJump = true;
-        }
 
-        Debug.Log("Velocity-Y: " + rigidbody2D.velocity.y);
+        Vector2 vel = rigidbody2D.velocity;
+        vel.x = Mathf.Clamp(vel.x, -MAX_SPEED, MAX_SPEED);
+        rigidbody2D.velocity = vel;
     }
 
 
@@ -76,31 +75,31 @@ public class PlayerControl : MonoBehaviour
     }
 
 
-    Vector3 PhysicsTick(Vector3 force)
-    {
-        // Calculate forces
-        force = Vector3.ClampMagnitude(force, MAX_FORCE);
+    //Vector3 PhysicsTick(Vector3 force)
+    //{
+    //    // Calculate forces
+    //    force = Vector3.ClampMagnitude(force, MAX_FORCE);
 
-        // Calculate drag
-        Vector3 drag = -DragCoefficient * m_velocity;
+    //    // Calculate drag
+    //    Vector3 drag = -DragCoefficient * m_velocity;
 
-        // Calculate gravity
-        Vector3 gravity = Physics.gravity;
+    //    // Calculate gravity
+    //    Vector3 gravity = Physics.gravity;
 
-        // Calculate final force
-        Vector3 resultantForce = force + drag + gravity;
+    //    // Calculate final force
+    //    Vector3 resultantForce = force + drag + gravity;
 
-        // Calculate acceleration
-        m_acceleration = resultantForce / Mass;
+    //    // Calculate acceleration
+    //    m_acceleration = resultantForce / Mass;
 
-        // Calculate velocity
-        Vector3 newVelocity = m_velocity + m_acceleration;
+    //    // Calculate velocity
+    //    Vector3 newVelocity = m_velocity + m_acceleration;
 
-        newVelocity = Vector3.ClampMagnitude(newVelocity, MAX_SPEED);
+    //    newVelocity = Vector3.ClampMagnitude(newVelocity, MAX_SPEED);
 
-        // Reset acceleration
-        m_acceleration = Vector3.zero;
+    //    // Reset acceleration
+    //    m_acceleration = Vector3.zero;
 
-        return new Vector3(newVelocity.x, newVelocity.y, 0.0f);
-    }
+    //    return new Vector3(newVelocity.x, newVelocity.y, 0.0f);
+    //}
 }
